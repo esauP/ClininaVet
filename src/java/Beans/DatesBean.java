7ae724b0ca2c3e8bbc0ebafc05a565cdf60406be
mjbenitez <mjb.dam@gmail.com>
2017-06-14 00:04:43 +0200

    14/06 00:01 Cambios para la administración

--- src/java/Beans/DatesBean.java
+++ src/java/Beans/DatesBean.java
@@ -1,11 +1,13 @@
 
 package Beans;
 
+import Controller.LPerson;
 import Controller.LPets;
 import Controller.Ldate;
 import java.io.IOException;
 import java.io.Serializable;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Locale;
@@ -15,6 +17,7 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.context.FacesContext;
 import javax.inject.Named;
 import pojo.Dates;
+import pojo.Person;
 import pojo.Pets;
 
 //Anotaciones para que el xhtml pueda operar con ella
@@ -33,7 +36,19 @@ public class DatesBean implements Serializable{
     private List<Dates> listacitas;
     private String idper;
     private Dates dateD = new Dates();
-    
+    private List<Pets> listamascotasPers;
+    private List<Person> listapersonas;
+       
+    /**
+     * Creates a new instance of PetsBean
+     *
+     * @throws java.sql.SQLException
+     */
+    public DatesBean() throws SQLException {
+        listapersonas = LPerson.getPeople();
+        this.listacitas = Ldate.getDatesDay();
+
+    }
     
     /**
      * Método que filtra las citas según el día
@@ -80,6 +95,56 @@ public class DatesBean implements Serializable{
         FacesContext.getCurrentInstance().addMessage(null, message);
         System.out.println("Fijar Mascota");
     }
+    
+    /**
+     * Metodo para eliminar una cita
+     *
+     * @param idDate
+     * @throws SQLException
+     * @throws IOException
+     */
+    public void deleteDate(int idDate) throws SQLException, IOException {
+
+        Boolean res;
+        FacesMessage msg;
+        res = Ldate.deleteDate(idDate);
+        if (res) {
+            msg = new FacesMessage("Cita eliminada para: " + dateD.getNamepet());
+        } else {
+            msg = new FacesMessage("Cita no eliminada para: " + dateD.getNamepet()+ " Ha surgido un problema");
+        }
+
+        FacesContext.getCurrentInstance().addMessage(null, msg);
+        FacesContext.getCurrentInstance().getExternalContext().redirect("faces/dates.xhtml");
+
+    }
+    
+    /**
+     * Metodo para autocompletar los id de las personas
+     *
+     * @param text
+     * @return List String
+     */
+    public List<String> AutocompletarID(String text) {
+        // Assumed search using the startsWith
+        List<String> queried = new ArrayList<>();
+        for (Person person : this.listapersonas) {
+            String namePer = person.getNamePer();
+            if (namePer.toLowerCase().startsWith(text) || namePer.startsWith(text)) {
+                queried.add(person.getIdperson());
+            }
+        }
+        return queried;
+    }
+    
+      /**
+     * Método para carga un listado de mascotas asociados al id de una persona
+     * @param IdPersona
+     * @throws SQLException 
+     */
+    public void BuscaMascotas(String IdPersona) throws SQLException {
+        listamascotasPers = LPets.getPets(IdPersona);
+    }
 
     public String getIdper() {
         return idper;
@@ -87,10 +152,6 @@ public class DatesBean implements Serializable{
 
     public void setIdper(String idper) {
         this.idper = idper;
-    }
-
-    public DatesBean() {
-        this.listacitas = Ldate.getDatesDay();
     }
 
     public int getId() {
@@ -148,28 +209,29 @@ public class DatesBean implements Serializable{
     public void setListacitas(List<Dates> listacitas) {
         this.listacitas = listacitas;
     }
+  
+    public Dates getDateD() {
+        return dateD;
+    }
+
+    public void setDateD(Dates dateD) {
+        this.dateD = dateD;
+    }
+
+    public List<Pets> getListamascotasPers() {
+        return listamascotasPers;
+    }
+
+    public void setListamascotasPers(List<Pets> listamascotasPers) {
+        this.listamascotasPers = listamascotasPers;
+    }
     
-    /**
-     * Metodo para eliminar una cita
-     *
-     * @param idDate
-     * @throws SQLException
-     * @throws IOException
-     */
-    public void deleteDate(int idDate) throws SQLException, IOException {
+    public List<Person> getListapersonas() {
+        return listapersonas;
+    }
 
-        Boolean res;
-        FacesMessage msg;
-        res = Ldate.deleteDate(idDate);
-        if (res) {
-            msg = new FacesMessage("Cita eliminada para: " + dateD.getNamepet());
-        } else {
-            msg = new FacesMessage("Cita no eliminada para: " + dateD.getNamepet()+ " Ha surgido un problema");
-        }
-
-        FacesContext.getCurrentInstance().addMessage(null, msg);
-        FacesContext.getCurrentInstance().getExternalContext().redirect("faces/dates.xhtml");
-
+    public void setListapersonas(List<Person> listapersonas) {
+        this.listapersonas = listapersonas;
     }
 
 }
